09f58932849600ebb9b66a9f2f4bd61b8265109c
Hamza <hamzabenhabib.hbh@gmail.com>
2026-01-12 13:10:44 +0100

    Fix Chatbot v3

--- src/AIChat/aichat.module.ts
+++ src/AIChat/aichat.module.ts
@@ -4,12 +4,14 @@ import { AIChat, AIChatSchema } from './aichat.schema';
 import { AIChatService } from './aichat.service';
 import { AIChatController } from './aichat.controller';
 import { AuthModule } from '../Auth/auth.module';
+import { Internship, InternshipSchema } from '../Internship/internship.schema';
 
 @Module({
   imports: [
     AuthModule,
     MongooseModule.forFeature([
-      { name: AIChat.name, schema: AIChatSchema }
+      { name: AIChat.name, schema: AIChatSchema },
+      { name: Internship.name, schema: InternshipSchema } // Add this line
     ]),
   ],
   providers: [AIChatService],
